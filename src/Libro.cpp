d73a3dbaf841b7fa717770009d1c5fb44c6d8200
mgarrido7 <mgarrido7>
2025-03-20 14:27:40 -0300

    Actualizada Libro.cpp

--- src/Libro.cpp
+++ src/Libro.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "Libro.h"
 
 Libro :: Libro(){
@@ -37,3 +38,44 @@ bool Libro::contienePalabra(std::string palabra){
     return false; 
 }
 
+=======
+#include "Libro.h"
+
+Libro :: Libro(){
+}
+
+Libro :: ~Libro(){
+}
+
+Libro::Libro(int identificador, DTFecha fecha, std::string editorial, std::set<std::string> palabrasDestacadas) {
+    this->editorial = editorial;
+    this->palabrasDestacadas = palabrasDestacadas; 
+}
+
+std::string Libro::getEditorial() {
+    return editorial;
+}
+
+std::set<std::string> getPalabrasDestacadas(){
+    return palabrasDestacadas;
+
+};
+
+
+void Libro::setPalabrasDestacadas(std::set<std::string> nuevasPalabras) {
+    palabrasDestacadas = nuevasPalabras;
+} 
+
+void Libro::setEditorial(std::string nuevaEditorial) {
+    editorial = nuevaEditorial;
+}
+
+bool contienePalabra(std::string palabra){
+    for ( std::string destacada : palabrasDestacadas) { // recorre todos los elementos del conjunto palabrasDestacadas. En cada iteracion destacada tomara el valor de una palabra dentro de palabrasDestacadas. Desventaja crea una copia  si usas const &auto detacada hace una referencia
+        if (destacada == palabra) { 
+            return true; 
+        }
+    }
+    return false; 
+}
+>>>>>>> 901bc37 (Actualizada Libro.cpp)
