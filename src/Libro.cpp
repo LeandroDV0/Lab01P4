6d036c43a8d68b47d1a1eff4eda9e69160c12f5c
juana-fidalgo <juanafico.jfc@gmail.com>
2025-03-21 11:44:25 -0300

    Faltaba un libro :: Libro.cpp

--- src/Libro.cpp
+++ src/Libro.cpp
@@ -28,7 +28,7 @@ void Libro::setEditorial(std::string nuevaEditorial) {
     editorial = nuevaEditorial;
 }
 
-bool contienePalabra(std::string palabra){
+bool Libro::contienePalabra(std::string palabra){
     for  (const auto& destacada : palabrasDestacadas) { // recorre todos los elementos del conjunto palabrasDestacadas. En cada iteracion destacada tomara el valor de una palabra dentro de palabrasDestacadas. Desventaja crea una copia  si usas const &auto detacada hace una referencia
         if (destacada == palabra) { 
             return true; 
