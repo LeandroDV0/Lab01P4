d155d016f3c13ccc9fc303f954ac697b7033757a
Federica-Mari <fedemaritorres@gmail.com>
2025-03-21 14:38:30 -0300

    Update ArticuloRevista.cpp

--- src/ArticuloRevista.cpp
+++ src/ArticuloRevista.cpp
@@ -9,7 +9,7 @@ ArticuloRevista :: ~ArticuloRevista(){
 
 }
 
-ArticuloRevista :: ArticuloRevista(std::string rev, std::string ext){
+ArticuloRevista :: ArticuloRevista(std::string DOI, std::string titulo, DTFecha fecha, std::string rev, std::string ext): Publicion(DOI, titulo, fecha){
     this->revista = rev;
     this->extracto = ext;
 }
