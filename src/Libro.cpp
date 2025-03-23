a07add760b7c0623f36bed094c83e50ebb8f3108
mgarrido7 <mgarrido7>
2025-03-23 16:49:25 -0300

    resuelto

--- src/Libro.cpp
+++ src/Libro.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "Libro.h"
 
 Libro :: Libro(){
@@ -52,3 +53,43 @@ bool Libro:: contienePalabra (std::string palabra){
 // si no podemos hacer strin.upper entonces hay que hacerlo con ASCII
     
 >>>>>>> 901bc37 (Actualizada Libro.cpp)
+=======
+#include "Libro.h"
+
+Libro :: Libro(){
+}
+
+Libro :: ~Libro(){
+}
+
+Libro::Libro(std::string editorial, std::set<std::string> palabrasDestacadas) {
+    this->editorial = editorial;
+    this->palabrasDestacadas = palabrasDestacadas; 
+}
+
+std::string Libro::getEditorial() {
+    return editorial;
+}
+
+std::set<std::string> getPalabrasDestacadas(){
+    return palabrasDestacadas;
+
+};
+
+void Libro::setPalabrasDestacadas(std::set<std::string> nuevasPalabras) {
+    palabrasDestacadas = nuevasPalabras;
+} 
+
+void Libro::setEditorial(std::string nuevaEditorial) {
+    editorial = nuevaEditorial;
+}
+
+bool Libro::contienePalabra(std::string palabra){
+    for  (const auto& destacada : palabrasDestacadas) { // recorre todos los elementos del conjunto palabrasDestacadas. En cada iteracion destacada tomara el valor de una palabra dentro de palabrasDestacadas. Desventaja crea una copia  si usas const &auto detacada hace una referencia
+        if (destacada == palabra) { 
+            return true; 
+        }
+    }
+    return false; 
+}
+>>>>>>> 42c9f2f (Actualizada Libro.cpp)
