b7adbd36b4715340dd6245e3cde7bc8f53e03bb4
Federica-Mari <fedemaritorres@gmail.com>
2025-03-24 15:11:51 -0300

    Update Libro.cpp

--- src/Libro.cpp
+++ src/Libro.cpp
@@ -7,7 +7,7 @@ Libro :: Libro(){
 Libro :: ~Libro(){
 }
 
-Libro::Libro(std::string DOI, std::string titulo, DTFecha fecha, std::string editorial, std::set<std::string> palabrasDestacadas): Publicion(DOI, titulo, fecha) {
+Libro::Libro(std::string DOI, std::string titulo, DTFecha fecha, std::string editorial, std::set<std::string> palabrasDestacadas): Publicacion(DOI, titulo, fecha) {
     this->editorial = editorial;
     this->palabrasDestacadas = palabrasDestacadas; 
 }
